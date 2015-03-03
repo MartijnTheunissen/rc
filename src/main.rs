3a8b8c61301f940be9a40468646cf4d9aa2bd594
Mika Attila <radiantstatue@gmail.com>
2015-03-03 10:16:46 +0100

    Accept expressions from command line

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,14 @@ fn main() {
     let mut reader = stdio::stdin();
     let mut calc = calc::Calc::new();
 
+    let input =
+        std::env::args().skip(1).fold(String::new(), |a, b| a + " " + &b);
+
+    if !input.is_empty() {
+        calc.evaluate(&input);
+        return;
+    }
+
     for line_result in reader.lock().lines() {
         let text = line_result.unwrap();
         println!("= {}", calc.evaluate(text.trim()));
