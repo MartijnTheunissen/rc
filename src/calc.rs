bee276fba1cb7144a7e3a9d5911ec6e00a12554b
Mika Attila <radiantstatue@gmail.com>
2015-03-04 17:58:09 +0100

    Make Calc::eval_tokens take an iterator, add some tests

--- src/calc.rs
+++ src/calc.rs
@@ -32,8 +32,8 @@ impl Calc {
         }
     }
 
-    fn eval_tokens(&mut self, tokens: &[Token]) -> Result<NumType, Error> {
-        Ok(4.0)
+    fn eval_tokens<T>(&mut self, tokens: T) -> Result<NumType, Error> {
+        Err(Error::Unknown)
     }
 }
 
@@ -43,11 +43,44 @@ mod test {
     use tokens::*;
     use tokens::Token::*;
     use tokens::InfixOperatorType::*;
+    use tokens::Token::InfixOperator as I;
 
     #[test]
-    fn test_simple_addition() {
+    fn test_simple_arith_2() {
         let mut calc = Calc::new();
-        assert_eq!(calc.eval_tokens(&[Num(2.), InfixOperator(Add), Num(2.)]),
-                   Ok(4.0));
+        // 2 + 2 = 4
+        let t = vec![Num(2.), I(Add), Num(2.)];
+        assert_eq!(calc.eval_tokens(t.into_iter()), Ok(4.));
+    }
+    #[test]
+    fn test_simple_arith_n() {
+        let mut calc = Calc::new();
+        // 9 - 6 - 1 = 2
+        let t = vec![Num(9.), I(Sub), Num(6.), I(Sub), Num(1.)];
+        assert_eq!(calc.eval_tokens(t.into_iter()), Ok(2.));
+    }
+    #[test]
+    fn test_precedence() {
+        let mut calc = Calc::new();
+        // 9 + 4 * 2 = 17
+        let t = vec![Num(9.), I(Add), Num(4.), I(Mul), Num(2.)];
+        assert_eq!(calc.eval_tokens(t.into_iter()), Ok(17.));
+    }
+    #[test]
+    fn test_simple_parens() {
+        let mut calc = Calc::new();
+        // 3 * (2 + 4) = 18
+        let t = vec![Num(3.), I(Mul), LParen, Num(2.), I(Add), Num(4.),
+                     RParen];
+        assert_eq!(calc.eval_tokens(t.into_iter()), Ok(18.));
+    }
+    #[test]
+    fn test_nested_parens() {
+        let mut calc = Calc::new();
+        // 3 + 4 + 2 * 7 * (3 + (2 + 4) * 2) = 217
+        let t = vec![Num(3.), I(Add), Num(4.), I(Add), Num(2.), I(Mul),
+                     Num(7.), I(Mul), LParen, Num(3.), I(Add), LParen, Num(2.),
+                     I(Add), Num(4.), RParen, I(Mul), Num(2.), RParen];
+        assert_eq!(calc.eval_tokens(t.into_iter()), Ok(217.));
     }
 }
