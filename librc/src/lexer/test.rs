c7369806c622a8fcb5c5a9dfb4553827cb5fb234
Mika Attila <radiantstatue@gmail.com>
2017-05-15 22:43:44 +0200

    Add helper macro for defining tests

--- librc/src/lexer/test.rs
+++ librc/src/lexer/test.rs
@@ -1,37 +1,29 @@
 use self::TokenKind::*;
 use super::*;
 
-#[test]
-fn empty() {
-    assert_eq!(lex(""), Ok(vec![]));
+macro_rules! test {
+    ($name:ident, $text:expr, $($token:expr),*) => {
+        #[test]
+        fn $name() {
+            assert_eq!(lex($text), Ok(vec![$($token),*]))
+        }
+    };
 }
 
-#[test]
-fn numeric_literal_no_frac() {
-    assert_eq!(
-        lex("245"),
-        Ok(
-            vec![
-                Token {
-                    kind: NumLiteral(245.),
-                    span: (0, 3),
-                },
-            ]
-        )
-    );
-}
-
-#[test]
-fn numeric_literal_with_frac() {
-    assert_eq!(
-        lex("9234.3247"),
-        Ok(
-            vec![
-                Token {
-                    kind: NumLiteral(9234.3247),
-                    span: (0, 9),
-                },
-            ]
-        )
-    )
-}
+test!(empty, "",);
+test!(
+    numeric_literal_no_frac,
+    "245",
+    Token {
+        kind: NumLiteral(245.),
+        span: (0, 3),
+    }
+);
+test!(
+    numeric_literal_with_frac,
+    "9234.3247",
+    Token {
+        kind: NumLiteral(9234.3247),
+        span: (0, 9),
+    }
+);
