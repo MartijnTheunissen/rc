c023e0d0bb413590586fccabe6479cd7733f63b5
Mika Attila <radiantstatue@gmail.com>
2017-05-18 22:01:20 +0200

    Add test for assignment

--- librc/src/lexer/test.rs
+++ librc/src/lexer/test.rs
@@ -25,3 +25,14 @@ Minus, 16, 17,
 Identifier, 18, 21,
 Rparen, 23, 24
 );
+test!(assign: "a = 3 * (8.24 + 2.62)" ==
+Identifier, 0, 1,
+EqualsSign, 2, 3,
+NumLiteral(3.), 4, 5,
+Asterisk, 6, 7,
+LParen, 8, 9,
+NumLiteral(8.24), 9, 13,
+Plus, 14, 15,
+NumLiteral(2.62), 16, 20,
+Rparen, 20, 21
+);
